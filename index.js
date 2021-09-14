f8c25adc11c2482c764ee0d5fd9f3b4786f1b005
ahmed-com <ahmed0grwan@gmail.com>
2021-09-14 14:45:13 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const Signer = require('./Signer')
+
+const app = express()
+let signer;
+
+try {
+    signer = new Signer()
+} catch (error) {
+    console.log(error);
+    process.exit(1);
+}
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.post('/sign',(req,res,next)=>{
+    const signOptions = req.body.signOptions;
+    const document = req.body.document;
+
+    const signature = signer.signFile(document,signOptions);
+    res.status(200).json({signature});
+})
+
+app.listen(4999);

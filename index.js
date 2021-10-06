fa9e893c2d17678cc627fc6af5da4fcd3c5e7bf5
ahmed-com <ahmed0grwan@gmail.com>
2021-10-06 13:44:12 +0200

    changed the app structure

--- index.js
+++ index.js
@@ -1,55 +1,65 @@
 // require('dotenv').config();
-const inquirer = require('inquirer')
-const express = require('express')
-const bodyParser = require('body-parser')
-const Signer = require('./Signer')
+const inquirer = require('inquirer');
+const express = require('express');
+const bodyParser = require('body-parser');
+const Signer = require('./Signer');
 
-let __SCARDPIN__;
+// let __SCARDPIN__;
 
-const app = express()
-let signer;
+const app = express();
+// let signer;
 
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	next();
 });
 
-app.post('/sign',(req,res,next)=>{
-    const pin = req.headers.authorization;
-    if(pin !== __SCARDPIN__) res.status(403).json({message : "unauthorized"})
-    // const signOptions = req.body.signOptions;
-    const document = req.body.document;
-    const jsonStr = JSON.stringify(document);
+app.post('/sign', (req, res, next) => {
+	const pin = req.headers.authorization;
+	// if(pin !== __SCARDPIN__) res.status(403).json({message : "unauthorized"})
+	// const signOptions = req.body.signOptions;
+	const document = req.body.document;
+	const jsonStr = JSON.stringify(document);
+	let signature;
+	try {
+		const signer = new Signer(pin);
+		signature = signer.signFile(jsonStr /*,signOptions*/);
+	} catch (err) {
+		res.status(403).json({ message: 'unauthorized' });
+		return;
+	}
 
-    const signature = signer.signFile(jsonStr/*,signOptions*/);
-    res.status(200).json({signature});
-})
+	res.status(200).json({ signature });
+});
 
-inquirer
-  .prompt([
-    {
-      type: 'password',
-      message: 'Enter the smart card PIN',
-      name: 'PIN',
-      mask: '*',
-    }
-  ])
-  .then((answer) => {
-    __SCARDPIN__ = answer.PIN;
+app.listen(4999, () => {
+	console.log('running on port 4999');
+});
 
-    try {
-        signer = new Signer(__SCARDPIN__)
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
+// inquirer
+//   .prompt([
+//     {
+//       type: 'password',
+//       message: 'Enter the smart card PIN',
+//       name: 'PIN',
+//       mask: '*',
+//     }
+//   ])
+//   .then((answer) => {
+//     __SCARDPIN__ = answer.PIN;
 
-    app.listen(4999,()=>{
-        console.log('running on port 4999')
-    });
-  });
+//     try {
+//         signer = new Signer(__SCARDPIN__)
+//     } catch (error) {
+//         console.log(error);
+//         process.exit(1);
+//     }
+
+//     app.listen(4999,()=>{
+//         console.log('running on port 4999')
+//     });
+//   });
